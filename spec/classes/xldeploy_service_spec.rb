e3822b683697850758b1d45302a843d966be1cf8
superseb <seb@nospam.fake>
2014-07-03 21:10:32 +0200

    Added required function, fixed service spec test

--- spec/classes/xldeploy_service_spec.rb
+++ spec/classes/xldeploy_service_spec.rb
@@ -1,16 +1,13 @@
 require 'spec_helper'
 
-# TODO: check template content ..
-
 describe 'xldeploy::service' do
   let(:facts) { {:osfamily       => 'RedHat',
-                 :concat_basedir => '/var/tmp'} }
+                 :concat_basedir => '/var/tmp'}}
 
-  let(:params) { {:os_user       => 'xldeploy' } }
+  let(:params) { {:productname   => 'xldeploy' }}
 
   context 'default' do
     it {should contain_service('xldeploy').with_ensure('running')}
-    it {should contain_file('/etc/init.d/xldeploy').with_content(/RUNNINGUSER="xldeploy"/)}
   end
 end
 
