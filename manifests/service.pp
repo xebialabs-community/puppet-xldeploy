2dff1a2be172f71cad1b211d5b95562385ce9507
superseb <seb@nospam.fake>
2014-07-03 20:40:52 +0200

    Added service class

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,8 +1,10 @@
 # Class xldeploy::service
 #
 # This class manages the xldeploy service
-class xldeploy::service {
-  service { 'xldeploy':
+class xldeploy::service (
+  $productname = $deployit::productname,
+) {
+  service { $productname:
     ensure => running,
     enable => true,
   }
