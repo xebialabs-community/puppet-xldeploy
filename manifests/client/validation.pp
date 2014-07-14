6f2be2f5b317dcb4a7b318013e567f66970a46ab
wianvos <wvos@xebia.com>
2014-07-14 16:17:16 +0200

    fixed owner settings for xldeploy default properties

--- manifests/client/validation.pp
+++ manifests/client/validation.pp
@@ -14,32 +14,32 @@ class xldeploy::client::validation (
 ) {
   ## type validation
   # string validation
-  validate_string($xldeploy::version)
-  validate_string($xldeploy::os_user)
-  validate_string($xldeploy::os_group)
-  validate_string($xldeploy::admin_password)
-  validate_string($xldeploy::importable_packages_path)
-  validate_string($xldeploy::client_user_password)
-  validate_string($xldeploy::client_user_password_salt)
+  validate_string($xldeploy::client::version)
+  validate_string($xldeploy::client::os_user)
+  validate_string($xldeploy::client::os_group)
+  validate_string($xldeploy::client::admin_password)
+  validate_string($xldeploy::client::client::importable_packages_path)
+  validate_string($xldeploy::client::client_user_password)
+  validate_string($xldeploy::client::client_user_password_salt)
 
   # boolean validation
-  validate_bool(str2bool($xldeploy::server))
-  validate_bool(str2bool($xldeploy::ssl))
-  validate_bool(str2bool($xldeploy::client_sudo))
-  validate_bool(str2bool($xldeploy::use_exported_resources))
-  validate_bool(str2bool($xldeploy::use_exported_keys))
-  validate_bool(str2bool($xldeploy::enable_housekeeping))
-  validate_bool(str2bool($xldeploy::install_java))
-  validate_bool(str2bool($xldeploy::client_propagate_key))
+  validate_bool(str2bool($xldeploy::client::server))
+  validate_bool(str2bool($xldeploy::client::ssl))
+  validate_bool(str2bool($xldeploy::client::client_sudo))
+  validate_bool(str2bool($xldeploy::client::use_exported_resources))
+  validate_bool(str2bool($xldeploy::client::use_exported_keys))
+  validate_bool(str2bool($xldeploy::client::enable_housekeeping))
+  validate_bool(str2bool($xldeploy::client::install_java))
+  validate_bool(str2bool($xldeploy::client::client_propagate_key))
 
-  notice($xldeploy::cis)
+  notice($xldeploy::client::cis)
 
   # hash validation
-  validate_hash($xldeploy::cis)
-  validate_hash($xldeploy::memberships)
-  validate_hash($xldeploy::roles)
-  validate_hash($xldeploy::users)
-  validate_hash($xldeploy::server_plugins)
+  validate_hash($xldeploy::client::cis)
+  validate_hash($xldeploy::client::memberships)
+  validate_hash($xldeploy::client::roles)
+  validate_hash($xldeploy::client::users)
+  validate_hash($xldeploy::client::server_plugins)
 
   ## content validation
   # check validity of this module on the specific system
