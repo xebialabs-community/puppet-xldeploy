9f515154ea7f617ca7b44fd26acb8a109ab3365f
Joris De Winne <jdewinne@localhost.localdomain>
2014-09-18 13:18:02 +0200

    Fixed all tests

--- spec/classes/server/xldeploy_install_spec.rb
+++ spec/classes/server/xldeploy_install_spec.rb
@@ -18,10 +18,8 @@ describe 'xldeploy::server::install' do
                       :server_plugins              => { }
       }}
 
-      it {should contain_group('deployit').with_ensure('present')}
-      it {should contain_user('deployit').with_ensure('present').with_gid('deployit')}
-      it {should contain_file('/var/log/deployit').with_ensure('link').with_target('/opt/deployit/deployit-3.9.4-server/log')}
-      it {should contain_file('/etc/deployit').with_ensure('link').with_target('/opt/deployit/deployit-3.9.4-server/conf')}
+      it {should contain_file('log dir link').with_ensure('link').with_target('/opt/deployit/deployit-3.9.4-server/log')}
+      it {should contain_file('conf dir link').with_ensure('link').with_target('/opt/deployit/deployit-3.9.4-server/conf')}
       it {should contain_file('/etc/init.d/deployit').with_owner('root').with_group('root').with_mode('0700')}
       it {should contain_file('/opt/deployit/deployit-server').with_owner('deployit').with_group('deployit').with_ensure('link').with_target('/opt/deployit/deployit-3.9.4-server')}
       it {should contain_file('/opt/deployit/deployit-server/scripts').with_owner('deployit').with_group('deployit').with_ensure('directory')}
@@ -42,7 +40,6 @@ describe 'xldeploy::server::install' do
     }}
 
     it {should contain_file('/var/tmp/deployit-3.9.4-server.zip').with_source('modules/deployit/sources/deployit-3.9.4-server.zip')}
-    it {should contain_file('/opt/deployit').with_ensure('directory')}
     it {should contain_file('/opt/deployit/deployit-server').with_ensure('link')}
     it {should contain_exec('unpack server file').with({
                     :command => '/usr/bin/unzip /var/tmp/deployit-3.9.4-server.zip;/bin/cp -rp /var/tmp/deployit-3.9.4-server/* /opt/deployit/deployit-3.9.4-server',
@@ -71,24 +68,24 @@ describe 'xldeploy::server::install' do
     it {should contain_xldeploy_netinstall('https://tech.xebialabs.com/download/deployit/3.9.4/deployit-3.9.4-server.zip')}
   end
   context 'with install_license set to true and puppet file given' do
-    let(:params) {{:version                     => '3.9.4',
-                   :base_dir                    => '/opt/deployit',
+    let(:params) {{:version                     => '4.0.1',
+                   :base_dir                    => '/opt/xldeploy',
                    :tmp_dir                     => '/var/tmp',
-                   :os_user                     => 'deployit',
-                   :os_group                    => 'deployit',
+                   :os_user                     => 'xldeploy',
+                   :os_group                    => 'xldeploy',
                    :install_type                => 'download',
                    :download_user               => 'download',
                    :download_password           => 'download',
-                   :server_home_dir             => '/opt/deployit/deployit-server',
-                   :download_server_url         => 'https://tech.xebialabs.com/download/deployit/3.9.4/deployit-3.9.4-server.zip',
-                   :puppetfiles_xldeploy_source => 'modules/deployit/sources',
+                   :server_home_dir             => '/opt/xldeploy/xldeploy-server',
+                   :download_server_url         => 'https://tech.xebialabs.com/download/xl-deploy/4.0.1/xl-deploy-4.0.1-server.zip',
+                   :puppetfiles_xldeploy_source => 'modules/xldeploy/sources',
                    :install_license             => 'true',
-                   :license_source              => 'modules/deployit/file/deployit-license.lic',
+                   :license_source              => 'modules/xldeploy/file/deployit-license.lic',
                    :productname                 => 'deployit',
                    :server_plugins              => { }
     }}
 
-    it {should contain_file('/opt/deployit/deployit-server/conf/deployit-license.lic').with_source('modules/deployit/file/deployit-license.lic')}
+    it {should contain_file('/opt/xldeploy/xldeploy-server/conf/deployit-license.lic').with_source('modules/xldeploy/file/deployit-license.lic')}
   end
   context 'with install_license set to true and url given' do
     let(:params) {{:version                     => '4.0.1',
