41f1f18c94fc5b3124380ecf42d5cf6685b90fa3
wianvos <wvos@xebia.com>
2014-11-10 13:20:31 -0500

    pushing for a levent relief

--- manifests/server.pp
+++ manifests/server.pp
@@ -187,13 +187,14 @@ class xldeploy::server (
   }
 
   #class to setup shared stuff between cli and server installations
-  class{'xldeploy::shared_prereq':
-    base_dir     => $base_dir,
-    os_user      => $os_user,
-    os_group     => $os_group,
-    os_user_home => $server_home_dir,
-    install_java => $install_java,
-    java_home    => $java_home
+  if !defined(Class['xldeploy::shared_prereq']) {
+    class{'xldeploy::shared_prereq':
+      base_dir     => $base_dir,
+      os_user      => $os_user,
+      os_group     => $os_group,
+      os_user_home => $server_home_dir,
+      install_java => $install_java,
+      java_home    => $java_home
+    }
   }
-
 }
