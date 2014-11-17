c3e4a426dc0c03f2666625d8d1962ae78a0d95a7
wianvos <wvos@xebia.com>
2014-11-17 14:54:53 +0100

    debuggig

--- lib/puppet/provider/rest_provider.rb
+++ lib/puppet/provider/rest_provider.rb
@@ -122,6 +122,7 @@ class Puppet::Provider::XLDeployRestProvider < Puppet::Provider
     types={}
     doc = REXML::Document.new(rest_get "/deployit/metadata/type/#{name}")
     types[name]=Hash[doc.elements.to_a('/descriptor/property-descriptors/property-descriptor').map { |x| [x.attributes['name'], x] }]
+    return type[name]
   end
 
   def to_xml(id, type, properties)
