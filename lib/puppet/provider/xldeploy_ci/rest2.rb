e2cd436f27a8de6d425c94f4fa0e8441de38ba3a
wianvos <wvos@xebia.com>
2014-11-18 14:15:57 +0100

    debuggig

--- lib/puppet/provider/xldeploy_ci/rest2.rb
+++ lib/puppet/provider/xldeploy_ci/rest2.rb
@@ -20,7 +20,20 @@ Puppet::Type.type(:xldeploy_ci).provide :rest2 do
   end
 
   def properties
-    ci.actual_properties
+
+    ci_hash = ci.actual_properties
+
+    # Add unmanaged k/v pairs that XL Deploy returns to our properties.
+    # Otherwise these will be reset when updating any other property.
+    ci_hash.each do |k, v|
+      resource[:properties][k] = v unless resource[:properties].include? k
+
+      # Temporarily replace password properties as well, until we can
+      # encode passwords ourselves
+      resource[:properties][k] = v if (k == 'password' or k == 'passphrase') and v.start_with?('{b64}')
+    end
+    ci_hash
+    
   end
 
   def properties=(value)
