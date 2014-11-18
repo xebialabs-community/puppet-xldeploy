1d4fd895a58a3d236989c8be442955cfddab0d21
wianvos <wvos@xebia.com>
2014-11-18 12:41:06 +0100

    debuggig

--- lib/puppet/provider/xldeploy_ci/rest2.rb
+++ lib/puppet/provider/xldeploy_ci/rest2.rb
@@ -1,35 +1,38 @@
 require 'puppet_x/xebialabs/xldeploy/ci.rb'
 Puppet::Type.type(:xldeploy_ci).provide :rest2 do
 
-  def initialize(*args)
-    super
-    p resource
-    @ci = Ci.new(resource[:rest_url], resource[:id], resource[:type], resource[:properties])
-  end
+
   def create
 
     if resource[:discovery]
-      @ci.run_discovery(discovery_max_wait = resource[:discovery_max_wait])
+      ci.run_discovery(discovery_max_wait = resource[:discovery_max_wait])
     else
-      @ci.persist
+      ci.persist
     end
   end
 
   def destroy
-    @ci.destroy
+    ci.destroy
   end
 
   def exists?
-    @ci.exists?
+    ci.exists?
   end
 
   def properties
-    @ci.actual_properties
+    ci.actual_properties
   end
 
   def properties=(value)
     ci.persist
   end
 
+  private
 
+  def ci
+    @ci || @ci = get_ci
+  end
+  def get_ci
+    Ci.new(resource[:rest_url], resource[:id], resource[:type], resource[:properties])
+  end
 end
