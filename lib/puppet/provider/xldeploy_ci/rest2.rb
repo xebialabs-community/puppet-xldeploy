358ed570b125e09888543d927b41ee444b0f4e59
wianvos <wvos@xebia.com>
2014-11-19 15:57:58 +0100

    debuggig

--- lib/puppet/provider/xldeploy_ci/rest2.rb
+++ lib/puppet/provider/xldeploy_ci/rest2.rb
@@ -32,6 +32,7 @@ Puppet::Type.type(:xldeploy_ci).provide :rest2 do
       # encode passwords ourselves
       resource[:properties][k] = v if (k == 'password' or k == 'passphrase') and v.start_with?('{b64}')
     end
+    p resource[:properties]
     Hash[resource[:properties].sort]
   end
 
