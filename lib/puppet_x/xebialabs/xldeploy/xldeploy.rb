de35acd9238d7882436dffcb50a871b5aa58df49
wianvos <wvos@xebia.com>
2014-11-19 14:37:20 +0100

    debuggig

--- lib/puppet_x/xebialabs/xldeploy/xldeploy.rb
+++ lib/puppet_x/xebialabs/xldeploy/xldeploy.rb
@@ -54,7 +54,7 @@ class Xldeploy
   end
 
   def type_description
-    output = rest_get("metadata/type/#{type}")
+    output = rest_get("metadata/type/#{@type}")
     doc = REXML::Document.new output
     Hash[doc.elements.to_a('/descriptor/property-descriptors/property-descriptor').map { |x| [x.attributes['name'], x] }]
   end
