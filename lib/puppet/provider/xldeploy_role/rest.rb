96dc028ac92d4a33f751d6a850a2206566e85f28
wianvos <wvos@xebia.com>
2014-11-27 16:19:06 +0100

    refactored user, role, role_permissions and dictionary_entry

--- lib/puppet/provider/xldeploy_role/rest.rb
+++ lib/puppet/provider/xldeploy_role/rest.rb
@@ -80,8 +80,11 @@ Puppet::Type.type(:xldeploy_role).provide :rest do
 
   def to_hash(input)
     doc = REXML::Document.new input
-    p doc.elements
-    p doc.elements['string'].text
+   
+    doc.elements.each("/*/*") do |prop|
+      p prop.name
+      p prop.text
+    end
     {'string' => doc.elements['string'].text}
   end
   private
