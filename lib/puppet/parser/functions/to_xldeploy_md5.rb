a34fdb916934969a4aba7cb7a1187b849baf6724
wianvos <wvos@xebia.com>
2014-12-02 10:21:58 -0500

    working on the to_xldeploy_md5 function

--- lib/puppet/parser/functions/to_xldeploy_md5.rb
+++ lib/puppet/parser/functions/to_xldeploy_md5.rb
@@ -42,7 +42,7 @@ require File.join(File.dirname(__FILE__),'../../../puppet_x/xebialabs/xldeploy/'
     cache = {} if cache == nil
 
     unless cache.has_key?(passwordString)
-
+      p "testing to_xldeploy_md5"
       # check if deployit is reachable
 
       pw = Password.new(restUrl, passwordString, ssl, verify_ssl)
