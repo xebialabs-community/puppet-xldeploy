3d07acaced4f7c03b1e10bb3a36593f7f3b65c5d
wianvos <wvos@xebia.com>
2014-12-15 10:15:05 -0500

    typo

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -84,7 +84,7 @@ class xldeploy::server::install (
     -> Anchor['server::postinstall']
   }
     'download'    : {
-      notice("xld_community_edition: ${xld_community_edition}")
+      notify{"xld_community_edition: ${xld_community_edition}": withpath => true} ->
       if str2bool($xld_community_edition) == false {
         Xldeploy_netinstall{
           user           => $download_user,
