2e83efc550b07f500ee49c02f3943aa6749876d8
wianvos <wvos@xebia.com>
2015-04-07 08:09:13 -0400

    fiddeling around with autorequires

--- lib/puppet/type/xldeploy_ci.rb
+++ lib/puppet/type/xldeploy_ci.rb
@@ -46,6 +46,7 @@ Puppet::Type.newtype(:xldeploy_ci) do
     # select all dictionaries to be required if the current type is a environment
     required = required + catalog.resource_refs.select {|ref| ref.has_key(type) and ref.type =~ /udm.dictionary/i } if self.type =~ /udm.Environment/i
     p self.type
+    p self.params
     if self.type =~ /udm.Environment/i
         p 'test'
         catalog.resource_refs.each {|ref| p catalog.resource(ref)}
