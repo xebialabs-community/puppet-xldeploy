029e23c2e56f3da2282b2f2c4bea78c0d8445f01
wianvos <wvos@xebia.com>
2015-06-08 16:50:17 +0200

    fixed tests

--- spec/classes/server/xldeploy_repository_spec.rb
+++ spec/classes/server/xldeploy_repository_spec.rb
@@ -23,42 +23,7 @@ describe 'xldeploy::server::repository' do
      }) }
 
     end
-  context 'with repository_type set to database' do
-    let(:params) { {
-        :os_user                           => 'xldeploy',
-        :os_group                          => 'xldeploy',
-        :server_home_dir                   => '/opt/xldeploy/xldeploy-server',
-        :repository_type                   => 'database',
-        :datastore_url                     => 'jdbc:postgresql://localhost:5432/xldeploy',
-        :datastore_user                    => 'xldeploy',
-        :datastore_password                => 'xldeploy',
-        :datastore_databasetype            => 'postgres',
-        :datastore_schema                  => 'postgres',
-        :datastore_persistencemanagerclass => 'org.apache.jackrabbit.core.persistence.pool.PostgreSQLPersistenceManager',
-    } }
-    it { should contain_file('/opt/xldeploy/xldeploy-server/conf/jackrabbit-repository.xml').with({
-        :ensure => 'present',
-        :owner => 'xldeploy',
-        :group => 'xldeploy',
-        :mode => '0640'
-    }) }
-  end
-
-  context 'with datastore_jdbc_driver_url set to a http address and repository_type set to database' do
-
-    let(:params) { {  :repository_type          => 'database',
-                      :datastore_jdbc_driver_url => 'http://some.driver.url/some_driver.jar',
-                      :server_home_dir          => '/opt/xldeploy/xldeploy-server',
-                      :os_group                 => 'xl-deploy',
-                      :os_user                  => 'xl-deploy'
-    } }
-
-    it { should contain_file('/opt/xldeploy/xldeploy-server/conf/jackrabbit-repository.xml')}
-    it { should contain_xldeploy_repo_driver_netinstall('http://some.driver.url/some_driver.jar').with_ensure('present').with_lib_dir('/opt/xldeploy/xldeploy-server/lib').with_owner('xl-deploy')}
-
-
-  end
-
+ 
   context 'with repository set to standalone' do
 
     let(:params) { {  :repository_type          => 'standalone',
@@ -73,20 +38,7 @@ describe 'xldeploy::server::repository' do
 
   end
 
-  context 'with datastore_jdbc_driver_url set to a puppet url and repository_type set to database' do
 
-    let(:params) { {  :repository_type          => 'database',
-                      :datastore_jdbc_driver_url => 'puppet:///some.driver.url/some_driver.jar',
-                      :server_home_dir          => '/opt/xldeploy/xldeploy-server',
-                      :os_group                 => 'xl-deploy',
-                      :os_user                  => 'xl-deploy'
-    } }
-
-    it { should contain_file('/opt/xldeploy/xldeploy-server/conf/jackrabbit-repository.xml')}
-    it { should contain_file('/opt/xldeploy/xldeploy-server/lib/some_driver.jar').with_owner('xl-deploy')}
-
-
-  end
 
 
 end
