407345189eff4d163a1ea4d168a52f981ffd3b32
Tomasz Nowodzinski <tomasz.nowodzinski@gmail.com>
2015-06-24 15:07:53 +0200

    Fixed local install - fixed rspec

--- spec/classes/server/xldeploy_install_spec.rb
+++ spec/classes/server/xldeploy_install_spec.rb
@@ -30,8 +30,26 @@ describe 'xldeploy::server::install' do
       it {should contain_file('/var/tmp/deployit-3.9.4-server.zip').with_source('puppet:///modules/xldeploy/xldeploy.zip')}
       it {should contain_file('/opt/deployit/deployit-server').with_ensure('link')}
       it {should contain_exec('unpack server file').with({
-                      :command => '/usr/bin/unzip /var/tmp/deployit-3.9.4-server.zip;/bin/cp -rp /var/tmp/deployit-3.9.4-server/* /opt/deployit/deployit-3.9.4-server',
+                      :command => '/usr/bin/unzip -o /var/tmp/deployit-3.9.4-server.zip;/bin/cp -rp /var/tmp/deployit-3.9.4-server/* /opt/deployit/deployit-3.9.4-server',
                       :creates => '/opt/deployit/deployit-3.9.4-server/bin',
+                      :cwd     => '/var/tmp',
+                      :user    => 'deployit'
+              }) }
+    end
+
+    context 'with community edition dowload_source set to a puppeturl' do
+      let :params do
+          default_params.merge({
+                  :download_server_url => 'puppet:///modules/xldeploy/xldeploy-free-edition.zip',
+                  :xld_community_edition => true
+                   })
+      end
+
+      it {should contain_file('/var/tmp/deployit-3.9.4-server-free-edition.zip').with_source('puppet:///modules/xldeploy/xldeploy-free-edition.zip')}
+      it {should contain_file('/opt/deployit/deployit-server').with_ensure('link')}
+      it {should contain_exec('unpack server file').with({
+                      :command => '/usr/bin/unzip -o /var/tmp/deployit-3.9.4-server-free-edition.zip;/bin/cp -rp /var/tmp/deployit-3.9.4-server-free-edition/* /opt/deployit/deployit-3.9.4-server-free-edition',
+                      :creates => '/opt/deployit/deployit-3.9.4-server-free-edition/bin',
                       :cwd     => '/var/tmp',
                       :user    => 'deployit'
               }) }
@@ -66,13 +84,30 @@ describe 'xldeploy::server::install' do
       it {should contain_file('/var/tmp/xldeploy-4.5.0-server.zip').with_source('puppet:///modules/xldeploy/xldeploy.zip')}
       it {should contain_file('/opt/xldeploy/xldeploy-server').with_ensure('link')}
       it {should contain_exec('unpack server file').with({
-                      :command => '/usr/bin/unzip /var/tmp/xldeploy-4.5.0-server.zip;/bin/cp -rp /var/tmp/xldeploy-4.5.0-server/* /opt/xldeploy/xldeploy-4.5.0-server',
+                      :command => '/usr/bin/unzip -o /var/tmp/xldeploy-4.5.0-server.zip;/bin/cp -rp /var/tmp/xldeploy-4.5.0-server/* /opt/xldeploy/xldeploy-4.5.0-server',
                       :creates => '/opt/xldeploy/xldeploy-4.5.0-server/bin',
                       :cwd     => '/var/tmp',
                       :user    => 'deployit'
               }) }
     end
 
+    context 'with community edition dowload_source set to a puppeturl' do
+      let :params do
+          default_params.merge({
+                          :download_server_url => 'puppet:///modules/xldeploy/xldeploy-free-edition.zip',
+                          :xld_community_edition => true
+                               })
+      end
+
+      it {should contain_file('/var/tmp/xldeploy-4.5.0-server-free-edition.zip').with_source('puppet:///modules/xldeploy/xldeploy-free-edition.zip')}
+      it {should contain_file('/opt/xldeploy/xldeploy-server').with_ensure('link')}
+      it {should contain_exec('unpack server file').with({
+                      :command => '/usr/bin/unzip -o /var/tmp/xldeploy-4.5.0-server-free-edition.zip;/bin/cp -rp /var/tmp/xldeploy-4.5.0-server-free-edition/* /opt/xldeploy/xldeploy-4.5.0-server-free-edition',
+                      :creates => '/opt/xldeploy/xldeploy-4.5.0-server-free-edition/bin',
+                      :cwd     => '/var/tmp',
+                      :user    => 'deployit'
+              }) }
+    end
 
     context 'with install_license set to true and puppet file given' do
       let :params do
